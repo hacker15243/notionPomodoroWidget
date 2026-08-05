5477444697b42887122bcfe0e61e05ff8ddb2ee7
leaf290 <2024873@apps.nsd.org>
2026-08-04 23:02:54 -0700

    update: basic pomodoro w/ session counting complete

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let timeLeft = 5 * 60;
+let workTime = 25 * 60;
+let breakTime = 5 * 60;
+
+let timeLeft = workTime;
+
+let isWorkTime = true;
 let timer = null;
 
+let sessionNumber = 1;
+
 const timerDisplay = document.getElementById("timer");
+const modeDisplay = document.getElementById("mode");
+const sessionDisplay = document.getElementById("session");
+
 const startButton = document.getElementById("start");
 const pauseButton = document.getElementById("pause");
 const resetButton = document.getElementById("reset");
 
+
 function updateDisplay() {
+
     let minutes = Math.floor(timeLeft / 60);
     let seconds = timeLeft % 60;
 
@@ -14,36 +26,81 @@ function updateDisplay() {
     seconds = String(seconds).padStart(2, "0");
 
     timerDisplay.textContent = `${minutes}:${seconds}`;
+
+    if (isWorkTime) {
+        modeDisplay.textContent = "session " + sessionNumber + ": Work";
+    } else {
+        modeDisplay.textContent = "session " + sessionNumber + ": Break";
+    }
+
+    sessionDisplay.textContent = `Session ${sessionNumber}`;
 }
 
+
 function startTimer() {
+
     if (timer !== null) {
         return;
     }
 
     timer = setInterval(function() {
-        if (timeLeft > 0) {
-            timeLeft--;
-            updateDisplay();
-        } else {
-            clearInterval(timer);
-            timer = null;
+
+        timeLeft--;
+
+        updateDisplay();
+
+        if (timeLeft <= 0) {
+            switchMode();
         }
+
     }, 1000);
 }
 
+
 function pauseTimer() {
+
     clearInterval(timer);
     timer = null;
 }
+
 
 function resetTimer() {
+
     clearInterval(timer);
     timer = null;
 
-    timeLeft = 5 * 60;
+    isWorkTime = true;
+    timeLeft = workTime;
+    sessionNumber = 1;
+
     updateDisplay();
 }
+
+
+function switchMode() {
+
+    if (isWorkTime) {
+
+        // Work finished
+        // Move to break
+
+        isWorkTime = false;
+        timeLeft = breakTime;
+
+    } else {
+
+        // Break finished
+        // Move to next work session
+
+        isWorkTime = true;
+        timeLeft = workTime;
+
+        sessionNumber++;
+    }
+
+    updateDisplay();
+}
+
 
 startButton.addEventListener("click", startTimer);
 pauseButton.addEventListener("click", pauseTimer);
